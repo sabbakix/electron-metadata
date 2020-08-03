a2ff97203b167593f13f7be7aab1f204370611d4
Stefy Soup <9255011+stefysab@users.noreply.github.com>
2020-08-03 16:35:03 +0200

    added render main ipc communication

--- electron-metadata/main.js
+++ electron-metadata/main.js
@@ -1,13 +1,44 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron')
+const util = require('util')
 const path = require('path')
+const fs = require('fs')
 
+const stat = util.promisify(fs.stat)
+
+// declare this as a variable globally so we can
+// reference it and so it will not be garbage collected
 let mainWindow
 
+// wait for the main process to be ready
 app.on('ready', () => {
-    const htmlPath =path.join('src','index.html')
+  // path to our html
+  const htmlPath = path.join('src', 'index.html')
 
-    mainWindow = new BrowserWindow()
-    mainWindow.loadFile(htmlPath)
-    
-    console.log('app ready')
+  // create a browser window
+  mainWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        }
+  })
+
+  mainWindow.loadFile(htmlPath)
+})
+
+// listen for files event by browser process
+ipcMain.on('files', async (event, filesArr) => {
+  try {
+    // asynchronously get the data for all the files
+    const data = await Promise.all(
+      filesArr.map(async ({ name, pathName }) => ({
+        ...await stat(pathName),
+        name,
+        pathName
+      }))
+    )
+
+    mainWindow.webContents.send('metadata', data)
+  } catch (error) {
+    // send an error event if something goes wrong
+    mainWindow.webContents.send('metadata:error', error)
+  }
 })
